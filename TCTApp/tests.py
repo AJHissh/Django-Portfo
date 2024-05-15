6abe3686997aedf2e3d3720733a3e8eccf8a440c
AJHissh <andrewhisshion@gmail.com>
2024-05-15 10:14:33 +0800

    initv1

--- TCTApp/tests.py
+++ TCTApp/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
+from . models import *
 
-# Create your tests here.
+
+class ModelTests(TestCase):
+
+    def setUp(self):
+          self.forms = Categoryone.objects.create()
